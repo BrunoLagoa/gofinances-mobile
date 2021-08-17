7cff2bc4a04ed9d07655acf4e14401af5c35fd7e
BrunoLagoa <miroldols@gmail.com>
2021-08-17 10:52:01 -0300

    Adicionando novos teste no component Perfil usando seletores

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -3,10 +3,28 @@ import { render } from '@testing-library/react-native';
 
 import { Profile } from '../../screens/Profile';
 
-test('check if show correctly user input name placeholder', () => {
+test('Check if show correctly user input name placeholder', () => {
   const { getByPlaceholderText } = render(<Profile />);
 
   const inputName = getByPlaceholderText('Nome');
 
   expect(inputName).toBeTruthy();
+});
+
+test('Check if user data has been loaded', () => {
+  const { getByTestId } = render(<Profile />);
+
+  const inputName = getByTestId('input-name');
+  const inputSurname = getByTestId('input-surname');
+
+  expect(inputName.props.value).toEqual('Bruno');
+  expect(inputSurname.props.value).toEqual('Castro');
+});
+
+test('Check if title render correctly', () => {
+  const { getByTestId } = render(<Profile />);
+
+  const textTitle = getByTestId('text-title');
+
+  expect(textTitle.props.children).toContain('Perfil');
 });
