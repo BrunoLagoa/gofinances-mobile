a9b6b11e471bf0805b676c905641b5b52bdb94c6
BrunoLagoa <miroldols@gmail.com>
2021-09-16 11:07:33 -0300

    Criado test auth hook

--- src/hooks/auth.spec.ts
+++ src/hooks/auth.spec.ts
@@ -1,45 +1,38 @@
-import 'jest-fetch-mock';
-
 import { renderHook, act } from '@testing-library/react-hooks';
-import { mocked } from 'ts-jest/utils';
-import { AuthProvider, useAuth } from './auth';
-import { startAsync } from 'expo-auth-session';
 import fetchMock from 'jest-fetch-mock';
 
-jest.mock('expo-auth-session');
+import { AuthProvider, useAuth } from './auth';
+
 fetchMock.enableMocks();
 
-describe('Auth Hook', () => {
-  it('should be able to sign in with Google account existing', async () => {
-    //Primeiro, nós precisamos do Token. Então, vamos mockar a função de startAsync.
-    const googleMocked = mocked(startAsync as any);
-    googleMocked.mockReturnValueOnce({
+const userTest = {
+  id: 'any_id',
+  email: 'john.doe@email.com',
+  name: 'John Doe',
+  photo: 'any_photo.png',
+};
+
+jest.mock('expo-auth-session', () => {
+  return {
+    startAsync: () => ({
       type: 'success',
       params: {
         access_token: 'any_token',
       },
-    });
+    }),
+  };
+});
 
-    //Agora que temos o Token, vamos mockar a requisição http dos dados de profile do usuário.
-    fetchMock.mockResponseOnce(
-      JSON.stringify({
-        id: 'any_id',
-        email: 'bruno@gmail.com',
-        name: 'Bruno',
-        photo: 'any_photo.png',
-      })
-    );
+describe('Auth Hook', () => {
+  it('should be able to sign in with Google account existing', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(userTest));
 
-    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+    const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
 
-    act(async () => await result.current.signInWithGoogle());
-    await waitForNextUpdate();
+    await act(() => result.current.signInWithGoogle());
 
-    // Você até pode usar esse console.log para visualizar os dados.
-    // console.log('USER PROFILE =>', result.current.user.email);
-
-    expect(result.current.user.email).toBe('bruno@gmail.com');
+    expect(result.current.user.email).toBe(userTest.email);
   });
 });
