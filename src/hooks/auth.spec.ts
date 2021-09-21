84990691bfb4b91df0107fb55fa1a7dc3b846d4f
BrunoLagoa <miroldols@gmail.com>
2021-09-21 11:40:11 -0300

    Adicionado cobertura de código

--- src/hooks/auth.spec.ts
+++ src/hooks/auth.spec.ts
@@ -47,4 +47,16 @@ describe('Auth Hook', () => {
 
     expect(result.current.user.id).toEqual('any_id');
   });
+
+  it('should be error with incorrectly Google parameters', async () => {
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    try {
+      await act(() => result.current.signInWithGoogle());
+    } catch (error) {
+      expect(result.current.user.id).toThrowError;
+    }
+  });
 });
